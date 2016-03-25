6169c738a6f70052d26bc7f6406f16544af07e0f
kaanburaksener <kaanburaksener@gmail.com>
2016-03-25 14:51:23 +0300

    Sign Up page has been added
    - Sign Up page has been added
    - Some improvements on UI
    - Fix some little bugs

--- app/src/main/java/com/kaanburaksener/bench/ui/SignupActivity.java
+++ app/src/main/java/com/kaanburaksener/bench/ui/SignupActivity.java
@@ -1,7 +1,20 @@
 package com.kaanburaksener.bench.ui;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import com.kaanburaksener.bench.R;
 
@@ -9,11 +22,83 @@ import com.kaanburaksener.bench.R;
  * Created by kaanburaksener on 24/03/16.
  */
 public class SignupActivity extends AppCompatActivity {
+    private TextView appName;
+    private EditText name;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
+        initializer();
+    }
+
+    /**
+     * This function is used to initialize the layout elements and the attributes of the class
+     */
+    private void initializer() {
+        appName = (TextView)findViewById(R.id.appName);
+        name = (EditText)findViewById(R.id.name);
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        setFont();
+        setStatusBarColor();
+    }
+
+    /**
+     * This function is used to change the status bar color
+     */
+    private void setStatusBarColor() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                getWindow().setStatusBarColor(getResources().getColor(R.color.color_primary_dark));
+            }
+        } catch (NumberFormatException e) {
+            Log.d("Status bar set error: ", e.toString());
+        }
+    }
+
+    /**
+     * This function is used to set font to the layout elements
+     */
+    private void setFont() {
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Ubuntu-M.ttf");
+        appName.setTypeface(typeface);
+    }
+
+
+    /**
+     * This function is used to redirect the user to sign up page
+     */
+    public void goSigninActivity(View view) {
+        Intent intent = new Intent(this, SigninActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * This function is used to check the validity of the input entered as email
+     */
+    public static boolean isEmailValid(String email) {
+        boolean isValid = false;
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+        if (matcher.matches()) {
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("Destroy", "Activity Gone");
+        super.onDestroy();
     }
 
 
